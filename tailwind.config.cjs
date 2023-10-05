ad0ef98b334e1a61fc6257f7d285bcdc88b9730f
anirbanbarman <anirbandali247@gmail.com>
2023-10-05 12:56:56 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: false,
+  content: ['./src/**/*.{svelte,html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
